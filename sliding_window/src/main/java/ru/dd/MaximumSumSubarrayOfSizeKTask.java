29363f420b3a8a9ec4a6828eedc7780b265cfe13
Denis Dyakin <dyakin.amm@gmail.com>
2025-02-12 14:08:58 +0300

    Solve MaximumSumSubarrayOfSizeKTask by bruteforce.

--- sliding_window/src/main/java/ru/dd/MaximumSumSubarrayOfSizeKTask.java
+++ sliding_window/src/main/java/ru/dd/MaximumSumSubarrayOfSizeKTask.java
@@ -23,8 +23,19 @@ public class MaximumSumSubarrayOfSizeKTask {
         int k2 = 2;
         maxSum = MaximumSumSubarrayOfSizeKTask.findMxSum(input2, k2);
         System.out.println(maxSum); //7
+
+        int [] input3 = {2, 1, 5, 1, 3, 2};
+        int k3 = 3;
+        maxSum = MaximumSumSubarrayOfSizeKTask.findMxSumBruteforce(input3, k3);
+        System.out.println(maxSum); //9
+
+        int [] input4 = {2, 3, 4, 1, 5};
+        int k4 = 2;
+        maxSum = MaximumSumSubarrayOfSizeKTask.findMxSumBruteforce(input4, k4);
+        System.out.println(maxSum); //7
     }
 
+    //O(N)
     private static int findMxSum(int[] input, int k) {
         int maxSum = 0;
 
@@ -40,6 +51,25 @@ public class MaximumSumSubarrayOfSizeKTask {
                 sum -= input[windowStart];
                 windowStart++;
             }
+        }
+
+        return maxSum;
+    }
+
+    //O(N*K)
+    private static int findMxSumBruteforce(int[] input, int k) {
+
+        int maxSum = 0;
+        int sum = 0;
+
+        for (int i = 0; i < input.length - k; i++) {
+            for (int j = 0; j < k; j++) {
+                sum += input[i + j];
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            sum = 0;
         }
 
         return maxSum;
