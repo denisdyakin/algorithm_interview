07f7ff2411ae7b48fb8b924a615217224c866e23
Denis Dyakin <dyakin.amm@gmail.com>
2025-02-15 14:09:57 +0300

    Solve Inserting Intervals task.

--- merge_intervals/src/main/java/ru/dd/MergeIntervals.java
+++ merge_intervals/src/main/java/ru/dd/MergeIntervals.java
@@ -67,7 +67,7 @@ public class MergeIntervals {
         input.add(new Interval(1, 4));
         input.add(new Interval(2, 5));
         input.add(new Interval(7, 9));
-        System.out.print("Merged intervals: ");
+        System.out.print("Merged intervals: "); //[[1,5], [7,9]]
         for (Interval interval : MergeIntervals.merge(input))
             System.out.print("[" + interval.start + "," + interval.end + "] ");
         System.out.println();
